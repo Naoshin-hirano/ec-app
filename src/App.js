7dd223a74a0f49ac3ab8b32345646cbb17ce0422
Hirano <hnaoya1120@gmail.com>
2022-01-18 00:11:56 +0900

    storeのstate取得できた

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch()
+  const selector = useSelector((state) => state)
+
+  console.log(selector.users)
+  console.log(dispatch)
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/reducks/store/initialState.js
@@ -0,0 +1,12 @@
+const initialState = {
+    product: {
+
+    },
+    users: {
+      isSignedIn: false,
+      uid: "",
+      username: ""
+    }
+};
+
+export default initialState
