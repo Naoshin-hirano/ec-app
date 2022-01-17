fc26410f0270d09ba5f52b5b486460d37f45f648
Hirano <hnaoya1120@gmail.com>
2022-01-17 23:48:28 +0900

    first commit

---
+++ src/reducks/users/reducers.js
@@ -0,0 +1,15 @@
+import * as Actions from './actions'
+import initialState from '../store/initialState'
+
+export const UsersReducer = (state = initialState.users, action) => {
+    switch (action.type) {
+        case Actions.SIGN_IN:
+            //...stateを...action.payloadへ上書き(...stateにあるけど...actions.payloadにない値があれば、...stateの値がそのままacitions.payloadに入る)
+            return {
+                ...state,
+                ...action.payload
+            }
+        default:
+            return state
+    }
+}
