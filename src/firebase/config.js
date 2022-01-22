d41c9d13808349a5bd5781331226474468158225
Hirano <hnaoya1120@gmail.com>
2022-01-22 15:52:40 +0900

    開発前にファイルの整理

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyAf0ZYSgQg3r_tO0aAC4c4HNaYWJaPVd1Y",
+    authDomain: "ecapp-ad7d7.firebaseapp.com",
+    projectId: "ecapp-ad7d7",
+    storageBucket: "ecapp-ad7d7.appspot.com",
+    messagingSenderId: "358762712390",
+    appId: "1:358762712390:web:6a0e825576748b9766e947",
+    measurementId: "G-272QV1WVEG"
+  };
