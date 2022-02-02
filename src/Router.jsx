7838b53ceda7574a8f0808a6671bc1cca10b3880
Hirano <hnaoya1120@gmail.com>
2022-02-02 17:53:54 +0900

    商品一覧表示とその削除・編集

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Switch } from 'react-router';
 //import { Home } from './Home'と１つ１つimportするのをtemplatesのindex.jsにまとめて書くことで防ぐ 
-import { Home, SignUp, SignIn, Reset, ProductEdit } from './templates';
+import { ProductList, SignUp, SignIn, Reset, ProductEdit } from './templates';
 import Auth from './Auth';
 
 const Router = () => {
@@ -12,7 +12,7 @@ const Router = () => {
             <Route exact path="/signin/reset" component={Reset} />
             
             <Auth>
-                <Route exact path="/" component={Home} />
+                <Route exact path="/" component={ProductList} />
                 <Route path="/product/edit(/:id)?" component={ProductEdit} />
             </Auth>
         </Switch>
--- src/reducks/products/operations.js
+++ src/reducks/products/operations.js
@@ -1,7 +1,34 @@
 import {db, FirebaseTimestamp } from "../../firebase";
 import {push} from 'connected-react-router';
+import {fetchProductsAction, deleteProductAction } from "./actions";
 
 const productsRef = db.collection('products')
+
+export const deleteProduct = (id) => {
+    return async (dispatch, getState) => {
+        productsRef.doc(id).delete()
+          .then(() => {
+              //getState: 現在のstateを取得できる
+              const prevProducts = getState().products.list;
+              const nextProducts = prevProducts.filter(product => product.id !== id)
+              dispatch(deleteProductAction(nextProducts))
+          })
+    }
+}
+
+export const fetchProducts = () => {
+    return async (dispatch) => {
+        productsRef.orderBy('updated_at', 'desc').get()
+         .then(snapshots => {
+             const productList = []
+             snapshots.forEach(snapshot => {
+                 const product = snapshot.data();
+                 productList.push(product)
+             })
+             dispatch(fetchProductsAction(productList))
+         })
+    }
+}
 
 export const saveProduct = (id, name, description, category, gender, price, images, sizes) => {
     return async (dispatch) => {
